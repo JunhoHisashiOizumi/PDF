c30642fdcbb95f9ba6c49ef73cd015dec9bb61ad
Oizumi Junho <99967568+JunhoHisashiOizumi@users.noreply.github.com>
2022-02-28 13:18:54 -0300

    AT

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require "vendor/autoload.php";
 
 use OizumiJunho\PDF\PDF;
 
-$html = "<h1 style='color: red;background: #000;'>Teste</h1>";
+$html = "<div style='width:300px;margin: 0 auto;background: #000;text-align: center;'><a href='https://www.google.com/' target='_blank' style='color: #fff;'>Teste</a></div>";
 
 PDF::ExePDF($html);
